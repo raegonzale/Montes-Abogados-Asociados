e1b66637814231cb7ce20643c24215eb8fbd6dc2
raegonzle <regl.850221@gmail.com>
2024-03-28 20:30:19 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.png';
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>Header si actualizo papi.</h1>
+      <img src={logo} alt="" />
+    </header>
+  )
+}
+
