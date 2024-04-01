cc973538a6b9eff1a0f6cf064d8b515963aa21c2
raegonzle <regl.850221@gmail.com>
2024-04-01 16:40:14 -0500

    NavBar3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header } from "./components/Header"
 function App() {
 
  return (
-   <main className="pt-2">
+   <main className="pt-3 w-auto mx-auto xl:w-[1280px]">
     <Header />
    </main>
  )  
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import logo from '../assets/logos/logo.png';
+
 import { NavBar } from './NavBar';
+
 
 export const Header = () => {
   return (
-    <header className='h-[65px] px-20 flex place-content-between items-center font-abc'>
-      <img className="xl:w-56 xl:h-13 hidden xl:flex" src={logo} alt="Logo principal" />
+    <header className='font-abc flex xl:h-[65px] xl:px-20 place-content-between items-center'>
+      <img className="xl:w-60 hidden xl:flex" src={logo} alt="Logo principal" />
       <NavBar />
     </header>
   )
